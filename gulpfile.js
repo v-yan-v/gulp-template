e9b8a7bb9cde1719306c0f1c3aa23416a9cb9054
Yan Vaidakovich <v.yan.v@yandex.ru>
2021-05-03 21:57:24 +0300

    feat: gulpfile - main tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 
-const { series, dest, src } = require('gulp')
+const { parallel, series, } = require('gulp')
 
-// const html = require('./gulp/tasks/concatHTML')
-const pug2html = require('./gulp/tasks/pug2html')
+const settings = require('./gulp/settings')
+
+const concatHTML = require('./gulp/tasks/concatHTML.js')
+const template2html = require('./gulp/tasks/template2html')
+const styles = require("./gulp/tasks/styles");
+const scripts = require("./gulp/tasks/scripts")
+const images = require("./gulp/tasks/images")
+const runServ = require("./gulp/tasks/server")
+const del = require("del")
+const fonts = require("./gulp/tasks/fonts")
 
 function setMode(isProduction = false) {
   return cb => {
@@ -12,6 +20,15 @@ function setMode(isProduction = false) {
   }
 }
 
+const doHTML = settings.engine === 'html' ? concatHTML : template2html
+
+const dev = parallel(doHTML, styles, images.minify, scripts, fonts)
+
+const clean = (cb) => {
+  // удаляем результирующую папку
+  return del(settings.output + '/**/*', {force: true}).then(() => {cb()})
+}
+
 /**
  * описываем "публичные" задачи
  * для этого прописываем их в экспорте присвоив им именованную функцию
@@ -19,5 +36,16 @@ function setMode(isProduction = false) {
  * module.exports.taskName = series(task1, task2, ...)
  * */
 
-// module.exports.doHtml = html
-module.exports.doPug = pug2html
+// separate tasks
+module.exports.doHtml = doHTML
+module.exports.doCSS = styles
+module.exports.doJS = scripts
+module.exports.doImg = images.minify
+module.exports.cleanImg = images.cleanImg
+module.exports.doFonts = fonts
+
+// complex tasks
+// module.exports.default = runServ
+module.exports.start = series(setMode(), clean, dev, runServ)
+module.exports.dev = dev
+module.exports.build = series(setMode(true), clean, dev)
